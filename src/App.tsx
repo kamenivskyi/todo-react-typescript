d1219c4bc2541eed96987091d5879e38b8f1971a
Roman Kamenivskyi <jumji19992412@gmail.com>
2020-10-26 23:11:17 +0200

    add addTodo functionality, create interfaces file

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,50 @@ import React, { useState } from 'react';
 
 import Navbar from './components/Navbar';
 import FormInput from './components/FormInput';
+import { ITodo } from './interfaces';
 
 import 'materialize-css/dist/css/materialize.min.css';
 
 import './App.css';
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const addTodo = value => {
-    // setTodos({});
+  const onAddTodo = (title: string) => {
+    if (title.trim().length > 3) {
+      const newTodo = createNewTodo(title);
+      
+      setTodos(prev => [...prev, newTodo]);
+    } else {
+      alert('New todo length must be greater than 3!')
+    }
+
   };
+
+  const createNewTodo = (title: string) => {
+    const newTodo: ITodo = { 
+      title,
+      id: Math.random(),
+      important: false,
+      done: false,
+    };
+
+    return newTodo; 
+  }
 
   return (
     <>
       <Navbar />
       <div className='container' style={{ marginTop: '2rem' }}>
-        <FormInput />
+        <FormInput onAddTodo={onAddTodo} />
+        <ul className="collection">
+          {todos.map((todo: ITodo) => {
+            console.log(todo);
+            return (
+              <li className='collection-item' key={todo.id}>{todo.title}</li>
+            )
+          })}
+        </ul>
       </div>
     </>
   );
--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 
-const FormInput = () => {
+interface FormInputProps {
+  onAddTodo(title: string): void;
+}
+
+const FormInput: React.FC<FormInputProps> = ({ onAddTodo }) => {
   const [title, setTitle] = useState<string>('');
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      onAddTodo(title);
+      
+      // console.log(title);
+      setTitle('');
+    }
+  };
 
   return (
     <div className='input-field'>
-      <input type='text' id='title' />
+      <input
+        type='text'
+        id='title'
+        placeholder='Введіть назву задачі'
+        value={title}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
       <label htmlFor='title' className='active'>
-        Введіть назви задачі
+        Введіть назву задачі
       </label>
     </div>
   );
